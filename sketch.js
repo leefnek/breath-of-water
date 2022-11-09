67877298b8a652ada38c5938c609da787fcc10a5
leefnek <rur.rur.xp@gmail.com>
2022-11-10 02:06:00 +0900

    feat: play background wave sounds

--- sketch.js
+++ sketch.js
@@ -1,4 +1,20 @@
-function setup() {
+let sound;
+
+function preload() {
+  soundFormats("mp3", "ogg");
+  sound = loadSound("./waves.mp3");
 }
 
-function draw() {}
+function setup() {
+  getAudioContext().suspend();
+  sound.play();
+}
+
+function draw() {
+  background(220);
+  textAlign(CENTER, CENTER);
+  text(getAudioContext().state, width / 2, height / 2);
+}
+function mousePressed() {
+  userStartAudio();
+}
