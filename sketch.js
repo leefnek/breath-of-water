b5ea730ca3f311d27522643e8ec625005c30ad23
leefnek <rur.rur.xp@gmail.com>
2022-11-11 01:25:27 +0900

    feat: display the waveform across the canvas

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 let sound;
+let fft;
+
+const G = 300;
 
 function preload() {
   soundFormats("mp3", "ogg");
@@ -6,15 +9,35 @@ function preload() {
 }
 
 function setup() {
-  getAudioContext().suspend();
-  sound.play();
+  // canvas setting
+  createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT();
 }
 
 function draw() {
-  background(220);
-  textAlign(CENTER, CENTER);
-  text(getAudioContext().state, width / 2, height / 2);
+  background(palette.black);
+  stroke(255);
+  //noFill();//안에 안채우게
+
+  // 시간 영역에 따른 진폭값을 담은 배열
+  const wave = fft.waveform();
+
+  beginShape();
+  for (let i = 0; i < width; i++) {
+    const index = floor(map(i, 0, width, 0, wave.length));
+
+    const x = i;
+    const y = wave[index] * G + height / 2;
+    vertex(x, y);
+  }
+  endShape();
 }
-function mousePressed() {
-  userStartAudio();
+function mouseClicked() {
+  if (sound.isPlaying()) {
+    sound.pause();
+    noLoop();
+  } else {
+    sound.play();
+    loop();
+  }
 }
