f80be6f6e98a12ca5b9e2ed9322b32da0273eddd
leefnek <rur.rur.xp@gmail.com>
2022-12-11 11:23:52 +0900

    fix: phase1

--- phase1/sketch.js
+++ phase1/sketch.js
@@ -1,9 +1,21 @@
 let sound, fft, peakDetect;
 let n = 3000;
 let i = 0;
-let a = [];
+let points = [];
 let r = 0;
-let b = [];
+let colorStep = 0;
+
+const color = [
+  "#025373",
+  "#03738C",
+  "#3FA8BF",
+  "#96D2D9",
+  "#346173",
+  "#A9CBD9",
+  "#04D9D9",
+  "#04D9C4",
+  "#6CD9CE",
+];
 
 function preload() {
   soundFormats("mp3", "ogg");
@@ -13,10 +25,10 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   for (i = 0; i < n * 2; i++) {
-    a[i] = random(660) - 330;
+    points[i] = random(660) - 330;
   }
   fft = new p5.FFT();
-  peakDetect = new p5.PeakDetect(500, 800);
+  peakDetect = new p5.PeakDetect(20, 300, 0.35, 20);
 }
 function draw() {
   if (!sound.isPlaying()) return;
@@ -25,18 +37,26 @@ function draw() {
 
   fft.analyze();
   peakDetect.update(fft);
-
   for (i = 0; i < n * 2 - 1; i++) {
     push();
-    translate(a[i], a[i + 1], 60 * sin(mag(a[i], a[i + 1]) / 30 - r));
-    stroke(0, random(100, 200), random(100, 250));
+    const tValue = sin(mag(points[i], points[i + 1]) / 30 - r);
+    translate(
+      points[i],
+      points[i + 1],
+      //15=>1=>172의 반복
+      60 * tValue
+    );
+    stroke(color[colorStep]);
     strokeWeight(5);
     point(0, 0);
-    //15=>1=>172의 반복
-    b.push(mag(a[i], a[i + 1]) / 30);
     pop();
   }
-  r += peakDetect.isDetected ? 0.6 : 0.1;
+  if (peakDetect.isDetected) {
+    r += 0.6;
+    colorStep = parseInt(random(color.length));
+  } else {
+    r += 0.1;
+  }
 }
 
 function keyPressed() {
